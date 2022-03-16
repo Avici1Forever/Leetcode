f2636946b76af525ee2771715250abbaa82902cb
Avici1Forever <450646975@qq.com>
2022-03-16 18:01:01 +0800

    fix

--- p9.cpp
+++ p9.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
-    bool isPalindrome(int x) {       
-        string str1,str2;
-        str1=to_string(x);
-        str2=to_string(x);
-        reverse(str1.begin(),str1.end());
-        if(str1==str2)
-            return true;
-        else
+    bool isPalindrome(int x) {
+        vector<int> digits;
+        if(x<0){
             return false;
+        }
+        while(x>0){
+            digits.push_back(x%10);
+            x=x/10;
+        }
+        for(int i=0;i<digits.size()/2;i++){
+            if(digits[i]!=digits[digits.size()-i-1]){
+                return false;
+            }
+        }
+        return true;
     }
 };
