cba172bd3cd53376d087aeca5c6339c6b69fcc87
Avici1Forever <450646975@qq.com>
2022-03-16 17:47:52 +0800

    fix

--- p1.cpp
+++ p1.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> v;
-        for(int i=0;i<nums.size()-1;i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i]+nums[j]==target){
-                    
-                    v.push_back(i);
-                    v.push_back(j);
-                    
+        for(int firstIndex=0;firstIndex<nums.size()-1;firstIndex++){
+            for(int secondIndex=firstIndex+1;secondIndex<nums.size();secondIndex++){
+                if(nums[firstIndex]+nums[secondIndex]==target){
+                    return {firstIndex,secondIndex};
                 }
             }
         }
-        return v;
+        return {};
     }
 };
